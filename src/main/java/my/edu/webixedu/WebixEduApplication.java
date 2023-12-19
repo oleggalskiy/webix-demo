6a323b07b264c811161083277d08107e376b1dd5
Oleg Galskiy <oleggalskiy@gmail.com>
2023-12-19 14:24:42 +0100

    init commit

---
+++ src/main/java/my/edu/webixedu/WebixEduApplication.java
@@ -0,0 +1,13 @@
+package my.edu.webixedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebixEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebixEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/my/edu/webixedu/WebixEduApplicationTests.java
@@ -0,0 +1,13 @@
+package my.edu.webixedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebixEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
